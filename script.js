50b052bd86814c442fb55ebe918c55c9ca7e63d1
dorjeezzz <dj.namjyal@gmail.com>
2024-08-03 19:53:30 -0600

    Completed dashboard

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ button.forEach((btn)=>{
         alert("these are not real buttons!\nthey don't do anything");
     })
 });
+
+const sideitem = document.querySelectorAll(".sidei");
+
+sideitem.forEach((itm)=>{
+    itm.addEventListener("mouseover", (e)=>{
+        itm.style.backgroundColor = "rgb(45, 135, 205)";
+    });
+    itm.addEventListener("mouseout", (e)=>{
+        itm.style.backgroundColor = "rgb(20, 143, 245)";
+    });
+});
